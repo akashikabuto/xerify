6ba4fb7a87bca5fa2debb7d31ed7071c9a0b8c2b
fidel <ramirezespinozafidel@gmail.com>
2024-03-18 15:13:28 -0500

    rollback_sslConfig

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,28 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { configure } from './__shared__/config/app.config';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
+
+  /*const httpsOptions = {
+    key: fs.readFileSync('/home/ec2-user/certs/xertify2023.key'),
+    cert: fs.readFileSync(
+      '/home/ec2-user/certs/xertify2023.crt',
+    ),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+    rawBody: true,
+  });
+
+  //Habilitar CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+   });*/
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
